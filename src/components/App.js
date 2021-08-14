f64fa67d5b96e0248b962b2b4a468b3e5d8b2aff
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-08-14 18:22:40 +0300

    deletes unuseful semicolon

--- src/components/App.js
+++ src/components/App.js
@@ -109,7 +109,7 @@ class App extends Component {
       return (
         <>
           <Searchbar onSubmit={formSubmitHandler} />
-          <GalleryLoader />;
+          <GalleryLoader />
         </>
       );
     }
